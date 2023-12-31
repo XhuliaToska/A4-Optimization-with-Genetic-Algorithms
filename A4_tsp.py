9a2dff0e9fe79216eb334d56173223e203081597
Xhulia Toska <116525724+XhuliaToska@users.noreply.github.com>
2023-12-31 11:09:46 +0100

    Write the funcation to calculate the total distance of a route.

--- A4_tsp.py
+++ A4_tsp.py
@@ -6,4 +6,13 @@ import matplotlib.pyplot as plt
 
 cities = tsplib95.load('ulysses16.tsp')
 
-print(cities)
+#print(cities)
+
+# Function to calculate the total distance of a route
+
+def total_distance_calculation(route, get_weight_fn):
+    total_distance =0
+    for i in range (len(route)-1):
+        total_distance += get_weight_fn(route[i],route[i+1])
+    total_distance +=get_weight_fn(route[-1], route[0]) # this line of code returns the starting city number
+    return total_distance
